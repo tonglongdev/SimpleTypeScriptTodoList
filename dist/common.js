171d69638c5248314064ff86b043169aa70e19b1
TongLongDev <tonglong.dev@gmail.com>
2025-07-02 18:20:24 +0700

    common

---
+++ dist/common.js
@@ -0,0 +1,12 @@
+"use strict";
+// // GENERIC
+// interface Example {
+//   name: string;
+//   age: number;
+// }
+// function testGeneric<T>(param: T[]): T[] {
+//   return param;
+// }
+// testGeneric<string>(["Long"]);
+// testGeneric<number>([123]);
+// testGeneric<Example>([{ name: "Long", age: 12 }]);
---
+++ src/common.ts
@@ -0,0 +1,15 @@
+// // GENERIC
+// interface Example {
+//   name: string;
+//   age: number;
+// }
+
+// function testGeneric<T>(param: T[]): T[] {
+//   return param;
+// }
+
+// testGeneric<string>(["Long"]);
+// testGeneric<number>([123]);
+// testGeneric<Example>([{ name: "Long", age: 12 }]);
+
+
